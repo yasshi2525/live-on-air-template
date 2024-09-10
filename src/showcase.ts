663cdcae442aaf20bd482dec91e7c1cf7c09035c
yasshi2525 <yasshy2@gmail.com>
2024-09-10 11:08:25 +0900

    LiveGameのサンプルを起動するよう修正

--- src/showcase.ts
+++ src/showcase.ts
@@ -1,7 +1,66 @@
-import { LiveOnAirScene, LiveOnAirSceneBuilder } from '@yasshi2525/live-on-air'
+import { LiveContext, LiveGame, LiveOnAirScene, LiveOnAirSceneBuilder } from '@yasshi2525/live-on-air'
 
 export const showcase = (experimental?: (scene: LiveOnAirScene) => void) => {
   const field = { x: 100, y: 100, width: g.game.width - 200, height: g.game.height - 200 }
+  LiveOnAirSceneBuilder.getDefault(g.game).spot({
+    liveClass: class extends LiveGame {
+      readonly font = new g.DynamicFont({
+        game: g.game,
+        fontFamily: 'sans-serif',
+        size: 50,
+        strokeColor: 'white',
+        strokeWidth: 4
+      })
+
+      protected evaluateScore (context: LiveContext): number {
+        return (context.vars as Record<string, g.FilledRect>).gauge.width / context.view.width * 100
+      }
+
+      protected handleGamePlay (context : LiveContext): () => void {
+        const { scene, view } = { ...context }
+        const gauge = new g.FilledRect({
+          scene,
+          parent: view,
+          width: 0,
+          height: 100,
+          cssColor: '#aa4444',
+          y: view.height / 2,
+          anchorY: 0.5
+        })
+        context.vars = { gauge }
+        gauge.onUpdate.add(() => {
+          gauge.width += view.width / 120
+          if (gauge.width > view.width) {
+            gauge.width = 0
+          }
+          gauge.modified()
+        })
+        return () => {
+          gauge.destroy()
+        }
+      }
+
+      protected handleIntroduction ({ scene, view }: LiveContext, next: () => void) {
+        view.append(new g.FilledRect({
+          scene,
+          width: view.width,
+          height: view.height,
+          cssColor: '#ffffaa',
+          opacity: 0.25
+        }))
+        const description = new g.Label({
+          scene,
+          parent: view,
+          text: 'ゲージの最大値でボタンを押そう！',
+          font: this.font
+        })
+        scene.setTimeout(() => next(), 3000)
+        return () => {
+          description.destroy()
+        }
+      }
+    }
+  })
   const sb = new LiveOnAirSceneBuilder(g.game)
     .layer({ field })
     .field({})

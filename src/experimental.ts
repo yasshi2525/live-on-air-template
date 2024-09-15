beb044806601d64a2c0ed9b222a70808f3144d52
yasshi2525 <yasshy2@gmail.com>
2024-09-15 18:48:45 +0900

    spot touchable の実装にあわせて experimental コードを削除

--- src/experimental.ts
+++ src/experimental.ts
@@ -2,16 +2,8 @@ import { showcase } from './showcase'
 import { LiveOnAirScene } from '@yasshi2525/live-on-air'
 
 export const experimental = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   showcase((scene: LiveOnAirScene) => {
-    for (const spot of scene.spots) {
-      spot.view.touchable = true
-      spot.view.onPointDown.add(() => {
-        if (spot.status === 'target') {
-          spot.unsetAsDestination()
-        } else if (spot.status === 'enabled') {
-          spot.setAsDestination()
-        }
-      })
-    }
+
   })
 }

124f7fd1dc8660a6d624d11b73f63e7a4c42c32b
yasshi2525 <yasshy2@gmail.com>
2024-09-17 06:50:35 +0900

    v1.0.0-alpha.0 対応

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { experimental } from './experimental'
 export const main = (param: GameMainParameterObject) => {
   g.game.vars.gameState = { score: 0 }
   g.game.random = param.random
-  g.game.vars.gameState.score = Math.floor(g.game.random.generate() * 1000)
 
   experimental()
 }
